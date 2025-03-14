f7083a3b6f2dff4fd58360f528fd5587bb06f3fc
vitorMalevichi07 <vmalevichi@gmail.com>
2025-03-14 09:59:49 -0300

    Criar a página do financeiro

---
+++ PHP/Financeiro.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Financeiro</title>
+</head>
+<body>
+    <?php
+        require 'header.php';
+        require 'sidebar.php';
+    ?>
+
+    <main>
+        <div class="titulo">
+            <h1><strong>Financeiro</strong></h1>
+        </div>
+    </main>
+</body>
+</html>
