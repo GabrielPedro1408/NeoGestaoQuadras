e2dbbf185945b149c468ab7cdd264be220c5d49b
vitorMalevichi07 <vmalevichi@gmail.com>
2025-03-16 10:11:11 -0300

    Criação do main de clientes.php

--- PHP/Clientes.php
+++ PHP/Clientes.php
@@ -24,6 +24,35 @@
             <h1><strong>Clientes</strong></h1>
         </div>
         <!-- end title -->
+        <!-- start principal -->
+        <div class="principal">
+            <!-- start "table"  -->
+            <div id="top-area">
+                <!-- start formPesquisar  -->
+                <div class="pesquisar">
+                    <form action="#" method="post">
+                        <label for="txtNome">Nome:</label>
+                        <input type="text" name="txtNome" id="txtNome">
+                        <label for="txtTel">Celular:</label>
+                        <input type="text" name="txtTel" id="txtTel">
+                        <label for="txtCpf">CPF:</label>
+                        <input type="text" name="txtCpf" id="txtCpf">
+                    </form>
+                </div>
+                <!-- end formPesquisar  -->
+                 <!-- start adicionarCli  -->
+                <div class="adicionar">
+                    <button type="button">+Adicionar</button><!-- colocar um pop-up aqui-->
+                </div>
+                <!-- end adiocionarCli  -->
+                <!-- start tableCli  -->
+                    <!-- Criar Tabela  -->
+                <!-- end tableCli  -->     
+            </div>
+            
+
+        </div>
+        <!-- end principal -->
     </main>
     <!-- end main -->
     
