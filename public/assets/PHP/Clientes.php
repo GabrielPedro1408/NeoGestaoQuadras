79a30f4d8ec4cf766f74e193c1cfa3894945e720
GabrielPedro1408 <gabrielspedro08@gmail.com>
2025-05-08 22:35:58 -0300

    inclusão de quase todos PopUp's na página de cliente
    Corrigi bugs da sidebar e também inseri quase todos os popUp's da tela de clientes, faltando somente o de informações

--- public/assets/PHP/Clientes.php
+++ public/assets/PHP/Clientes.php
@@ -5,11 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <link rel="shortcut icon" href="../images/financeiro.png" type="image/x-icon">
     <link rel="stylesheet" href="../CSS/clientes.css">
-    <link rel="stylesheet" href="../CSS/PopUp.css">
+    <link rel="stylesheet" href="../CSS/PopUpEditar.css">
     <link rel="stylesheet" href="../CSS/bootstrap.min.css">
     <link rel="stylesheet" href="../CSS/fontawesome.min.css">
     <script type="module" src="../JS/bootstrap.bundle.min.js"></script>
-    <script type="module" src="../JS/PopUp.js"></script>
+    <script type="module" src="../JS/PopUpBuscar.js"></script>
+    <script type="module" src="../JS/PopUpCadastro.js"></script>
+    <script type="module" src="../JS/PopUpEditar.js"></script>
+    <script type="module" src="../JS/PopUpExcluir.js"></script>
+    <script type="module" src="../JS/PopUpInfo.js"></script>
     <title>NPL Quadras</title>
 
 </head>
@@ -19,17 +23,239 @@
         require '../components/sidebar.php';
         require '../components/header.php' ;
     ?>
-
-    <!-- start main -->
-    <div class="modal" id="modal">
-                    <div class="modal-inner">
-                        <h2>Informações para Contato</h2>
-                        <h5>Email</h5> <p>neoconstrutora@yahoo.com.br</p>
-                        <h5>Telefone</h5> <p>+55 (011) 98765-0129</p>
-                        <h5>Endereço</h5> <p>Av. 9 de Julho, 3160, Judiaí - SP</p>
-                        <button id="closePopUp">Fechar</button>
-                    </div>
+    <!-- PopUps -->
+     <!-- cadastro cli -->
+    
+        <div class="modal" id="modalCadastro">
+            <div class="modal-inner">
+                <div class="top-pop-up">
+                    <h3>Cadastro de Cliente</h3>
+                    <a href="#" id="closePopUpCadastro" ><i class="fa-solid fa-x"></i></a>
                 </div>
+            <form class="row g-3" action="Clientes.php" method="POST">
+                <div class="col-md-6">
+                    <label for="nomeCli" class="form-label">Primeiro Nome</label>
+                    <input type="text" class="form-control" id="nomeCli">
+                </div>
+                <div class="col-md-6">
+                    <label for="sobrenomeCli" class="form-label">Sobrenome</label>
+                    <input type="text" class="form-control" id="sobrenomeCli">
+                </div>
+                <div class="col-md-4">
+                    <label for="dataNascCli" class="form-label">Data de Nascimento</label>
+                    <input type="date" class="form-control" id="dataNascCli">
+                </div>
+                <div class="col-md-4">
+                    <label for="cpfCli" class="form-label">CPF</label>
+                    <input type="int" class="form-control" id="cpfCli">
+                </div>
+                <div class="col-md-4">
+                    <label for="rgCli" class="form-label">RG (Registro Geral)</label>
+                    <input type="int" class="form-control" id="rgCli">
+                </div>
+                <div class="col-md-6">
+                    <label for="cepCli" class="form-label">CEP</label>
+                    <input type="int" class="form-control" id="cepCli">
+                </div>
+                <div class="col-md-6">
+                    <label for="cidadeCli" class="form-label">Cidade</label>
+                    <input type="text" class="form-control" id="cidadeCli">
+                </div>
+                <div class="col-md-6">
+                    <label for="ruaCli" class="form-label">Rua</label>
+                    <input type="text" class="form-control" id="ruaCli">
+                </div>
+                <div class="col-md-3">
+                    <label for="estadoCli" class="form-label">Estado</label>
+                    <input type="text" class="form-control" id="estadoCli">
+                </div>
+                <div class="col-md-3">
+                    <label for="casaCli" class="form-label">Nº Da Residência</label>
+                    <input type="int" class="form-control" id="casaCli">
+                </div>
+                <div class="col-md-6">
+                    <label for="contatoCli" class="form-label">Contato</label>
+                    <input type="int" class="form-control" id="contatoCli">
+                </div>
+                <div class="col-6">
+                    <label for="emailCli" class="form-label">Email</label>
+                    <input type="text" value="" class="form-control" id="emailCli">
+                </div>
+                <div class="col-md-12">
+                    <label for="descCli" class="form-label">Complementos</label>
+                    <input type="text" class="form-control" id="descCli">
+                </div>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary">Adicionar Cliente</button>
+                </div>
+                </form>
+            </div>
+        </div> 
+
+    <!-- buscar cli -->
+    <div class="modal" id="modalBuscar">
+            <div class="modal-inner">
+                <div class="top-pop-up">
+                    <h3>Informações do Cliente</h3>
+                    <a href="#" id="closePopUpBuscar" ><i class="fa-solid fa-x"></i></a>
+                </div>
+            <form class="row g-3" action="Clientes.php" method="POST">
+                <div class="col-md-6">
+                    <label for="nomeCli" class="form-label">Primeiro Nome</label>
+                    <input type="text" class="form-control" id="nomeCli">
+                </div>
+                <div class="col-md-6">
+                    <label for="sobrenomeCli" class="form-label">Sobrenome</label>
+                    <input type="text" class="form-control" id="sobrenomeCli">
+                </div>
+                <div class="col-md-4">
+                    <label for="dataNascCli" class="form-label">Data de Nascimento</label>
+                    <input type="date" class="form-control" id="dataNascCli">
+                </div>
+                <div class="col-md-4">
+                    <label for="cpfCli" class="form-label">CPF</label>
+                    <input type="int" class="form-control" id="cpfCli">
+                </div>
+                <div class="col-md-4">
+                    <label for="rgCli" class="form-label">RG (Registro Geral)</label>
+                    <input type="int" class="form-control" id="rgCli">
+                </div>
+                <div class="col-md-6">
+                    <label for="cepCli" class="form-label">CEP</label>
+                    <input type="int" class="form-control" id="cepCli">
+                </div>
+                <div class="col-md-6">
+                    <label for="cidadeCli" class="form-label">Cidade</label>
+                    <input type="text" class="form-control" id="cidadeCli">
+                </div>
+                <div class="col-md-6">
+                    <label for="ruaCli" class="form-label">Rua</label>
+                    <input type="text" class="form-control" id="ruaCli">
+                </div>
+                <div class="col-md-3">
+                    <label for="estadoCli" class="form-label">Estado</label>
+                    <input type="text" class="form-control" id="estadoCli">
+                </div>
+                <div class="col-md-3">
+                    <label for="casaCli" class="form-label">Nº Da Residência</label>
+                    <input type="int" class="form-control" id="casaCli">
+                </div>
+                <div class="col-md-6">
+                    <label for="contatoCli" class="form-label">Contato</label>
+                    <input type="int" class="form-control" id="contatoCli">
+                </div>
+                <div class="col-6">
+                    <label for="emailCli" class="form-label">Email</label>
+                    <input type="text" value="" class="form-control" id="emailCli">
+                </div>
+                <div class="col-md-12">
+                    <label for="descCli" class="form-label">Complementos</label>
+                    <input type="text" class="form-control" id="zdescCli">
+                </div>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary">Buscar Cliente</button>
+                </div>
+                </form>
+            </div>
+        </div> 
+
+    <!-- editar cli -->
+
+
+    <div class="modal" id="modalEditar">
+            <div class="modal-inner">
+                <div class="top-pop-up">
+                    <h3>Edição das Informações</h3>
+                    <a href="#" id="closePopUpEditar" ><i class="fa-solid fa-x"></i></a>
+                </div>
+            <form class="row g-3" action="Clientes.php" method="POST">
+                <div class="col-md-6">
+                    <label for="nomeCli" class="form-label">Primeiro Nome</label>
+                    <input type="text" class="form-control" id="nomeCli">
+                </div>
+                <div class="col-md-6">
+                    <label for="sobrenomeCli" class="form-label">Sobrenome</label>
+                    <input type="text" class="form-control" id="sobrenomeCli">
+                </div>
+                <div class="col-md-4">
+                    <label for="dataNascCli" class="form-label">Data de Nascimento</label>
+                    <input type="date" class="form-control" id="dataNascCli">
+                </div>
+                <div class="col-md-4">
+                    <label for="cpfCli" class="form-label">CPF</label>
+                    <input type="int" class="form-control" id="cpfCli">
+                </div>
+                <div class="col-md-4">
+                    <label for="rgCli" class="form-label">RG (Registro Geral)</label>
+                    <input type="int" class="form-control" id="rgCli">
+                </div>
+                <div class="col-md-6">
+                    <label for="cepCli" class="form-label">CEP</label>
+                    <input type="int" class="form-control" id="cepCli">
+                </div>
+                <div class="col-md-6">
+                    <label for="cidadeCli" class="form-label">Cidade</label>
+                    <input type="text" class="form-control" id="cidadeCli">
+                </div>
+                <div class="col-md-6">
+                    <label for="ruaCli" class="form-label">Rua</label>
+                    <input type="text" class="form-control" id="ruaCli">
+                </div>
+                <div class="col-md-3">
+                    <label for="estadoCli" class="form-label">Estado</label>
+                    <input type="text" class="form-control" id="estadoCli">
+                </div>
+                <div class="col-md-3">
+                    <label for="casaCli" class="form-label">Nº Da Residência</label>
+                    <input type="int" class="form-control" id="casaCli">
+                </div>
+                <div class="col-md-6">
+                    <label for="contatoCli" class="form-label">Contato</label>
+                    <input type="int" class="form-control" id="contatoCli">
+                </div>
+                <div class="col-6">
+                    <label for="emailCli" class="form-label">Email</label>
+                    <input type="text" value="" class="form-control" id="emailCli">
+                </div>
+                <div class="col-md-12">
+                    <label for="descCli" class="form-label">Complementos</label>
+                    <input type="text" class="form-control" id="descCli">
+                </div>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary">Confirmar Edição</button>
+                </div>
+                </form>
+            </div>
+        </div>
+
+
+
+        <div class="modal excluir" id="modalExcluir">
+            <div class="modal-inner">
+                <div class="top-pop-up-excluir">
+                    <h3>Deseja realmente excluir esse cadastro? </h3>
+                    <a href="#" id="closePopUpExcluir" ><i class="fa-solid fa-x"></i></a>
+                </div>
+            <form class="row g-3" action="Clientes.php" method="POST">
+                <div class="col-md-6">
+                    <label for="nomeCli" class="form-label">Primeiro Nome</label>
+                    <input type="text" value="bosta" class="form-control" id="nomeCli" disabled>
+                </div>
+                <div class="col-md-6">
+                    <label for="sobrenomeCli" class="form-label">Sobrenome</label>
+                    <input type="text" value="pietrinho" class="form-control" id="sobrenomeCli" disabled>
+                </div>
+                <div class="buttons">
+                    <button type="submit" id="excluirCli" class="deletar">Excluir</button>
+                    <button id="closePopUpExcluir" class="bt-cancelar" >Cancelar</button>
+                </div>
+                </form>
+            </div>
+        </div>
+
+                <!-- PopUps -->
+
+
     <div id="main-content">
         <main>
             <div class="container">
@@ -41,11 +267,11 @@
                 <div class="container-fluids">
                 <section class="top-area">
                     <div class="adicionar">
-                        <button type="button">+ Novo Cliente</button>
+                        <button id='openPopUpCadastro' type="button">+ Novo Cliente</button>
                     </div>
                             
                     <div class="pesquisar">
-                        <button  id="openPopUp" >Buscar : <i class="fa-solid fa-magnifying-glass"></i></button>
+                        <button id='openPopUpBuscar'>Buscar : <i class="fa-solid fa-magnifying-glass"></i></button>
                     </div>
                 </section>
                     <!-- start tableCli  -->
@@ -71,9 +297,9 @@
                                 <td><label for='cpfCli'>40090080023</label></td>
                                 <td><label for='enderecoCli'>bangalo do espeto</label></td>
                                 <td class='icons-item'>
-                                    <a id='openPopUp' href='#'><i  class='fa-solid fa-pen-to-square first'></i></a>
-                                    <a href='#'><i class='fa-solid fa-trash second'></i></a>
-                                    <a href='#'><i class='fa-solid fa-circle-info third'></i></a>
+                                    <a id='openPopUpEditar' href='#'><i  class='fa-solid fa-pen-to-square first'></i></a>
+                                    <a id='openPopUpExcluir'href='#'><i class='fa-solid fa-trash second'></i></a>
+                                    <a id='openPopUpInfo'href='#'><i class='fa-solid fa-circle-info third'></i></a>
                                 </td>";
                             // }
                             ?>
