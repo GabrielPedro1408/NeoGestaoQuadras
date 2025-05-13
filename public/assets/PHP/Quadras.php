a8efe2b8a284e9b5bdbb3793529ffc8f679c5ef5
DanielLPereira <daniellimapereira9@gmail.com>
2025-05-13 17:17:36 -0300

    Update Quadras.php

--- public/assets/PHP/Quadras.php
+++ public/assets/PHP/Quadras.php
@@ -2,50 +2,80 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/style.css">
-    <script type="module" src="../components/sidebar.js"></script>
-    <script type="module" src="../components/header.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <link rel="shortcut icon" href="../images/financeiro.png" type="image/x-icon">
+    <link rel="stylesheet" href="../CSS/quadras.css">
+    <link rel="stylesheet" href="../CSS/bootstrap.min.css">
+    <link rel="stylesheet" href="../CSS/fontawesome.min.css">
+    <script type="module" src="../JS/bootstrap.bundle.min.js"></script>
     <title>NPL Quadras</title>
 
 </head>
 <body>
-
-    <?php
+    <?php  
         require '../components/sidebar.php';
-        require '../components/header.php' ;
+        require '../components/header.php';
     ?>
 
     <!-- start main -->
-    <main>
+    <div id="main-content">
+        <div class="container">
+            <main>
+                <div class="titulo">
+                    <h1>Cadastro de Quadras</h1>
+                </div>
         <!-- start top page -->
-        <section class="top-page">
-        <div class="titulo">
-            <h1><strong>Quadras</strong></h1>
-        </div>
-        <div class="botao">
-            <button calss="botao-Adicionar">Adicionar</button>
-        </div>
-        </section>
+        <div class="top-page">
+            <div class="bts">
+                <button type="submit"href="#">+Nova quadra</button>
+                <input type="text" name="buscar" id="buscar"placeholder="Buscar:"<i class="fa-solid fa-magnifying-glass"></i>>
+            </div>
+            
+                 
+            
+            <section class="table">
+                <!-- start tableQuadras  -->
+                    <div class="table-quadra">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Disponibilidade</th>
+                                <th scope="col">Modalidade</th>
+                                <th scope="col">Valor Hora</th>
+                                <th scope="col">Ações</th>
+                                </tr>
+                            </thead>
+                            <tbody>                                    
+                            <?php
+                                echo"
+                                <td scope ='row'><label for='nomeQuadra'>Quadra Basquete</label></dh>
+                                <td><label for='disponibilidadeQua'>Disponivel</label></td>
+                                <td><label for='modalidadeQua'>Basquete</label></td>
+                                <td><label for='valorHora'>140R$</label></td>
+                                <td class='icons-item'>
+                                    <a id='openPopUpEditar' href='#'><i  class='fa-solid fa-pen-to-square first'></i></a>
+                                    <a id='openPopUpExcluir'href='#'><i class='fa-solid fa-trash second'></i></a>
+                                    <a id='openPopUpInfo'href='#'><i class='fa-solid fa-circle-info third'></i></a>
+                                </td>";
+                            ?>
+                            </tbody>
+                        </table>
+                        <div class="footer-table">
+                            <div class='esquerda'>
+                                <h3>Listando</h3>
+                            <?php 
+                                echo"
+                                <div class='labels'><label for='paginaAtual'>1</label> <p>/</p> <label for='totalPaginas'>7</label></div></div>
+                                <div class='direita'><a href='#'><i class='fa-solid fa-arrow-left'></i></a> <label for='paginaAtual'>1</label> <a href='#'><i class='fa-solid fa-arrow-right'></i></a></div>
+                                ";
+                            ?>
+                        </div>
+            </section>
+    </div>
             <!-- end top page -->
 
-            <!-- start bottom page -->
-            <section class="bottom-page">
-            <div class="quadra">
-            <form action="#" method="post">
-            <label for="nome-quadra"></label>
-            <label for="tipo-quadra"></label>
-            <label for="horarios"></label>
-            <label for="disponibilidade"></label>
-            </form>
-            <a href="EditarQuadra.php"><button><img src="../images/editar.png" alt=""></button></a>
-            <!-- pensar no botão de excluir -->
-            <a href="#"><button><img src="../images/excluir.png" alt=""></button></a>
-            </div>
-        </section>
-
-        <!-- end bottom page -->
+           
     </main>
     <!-- end main -->
 </body>
