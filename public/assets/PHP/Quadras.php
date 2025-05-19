4fe4b6792a753594bb87450c6e0d303f155ab1d3
raphaelpietro08 <raphaelpietro58@gmail.com>
2025-05-19 16:53:14 -0300

    mudanças popup quadra

--- public/assets/PHP/Quadras.php
+++ public/assets/PHP/Quadras.php
@@ -1,63 +1,86 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <link rel="shortcut icon" href="../images/financeiro.png" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/quadras.css">
+    <link rel="stylesheet" href="../CSS/PopUpEditar.css">
     <link rel="stylesheet" href="../CSS/bootstrap.min.css">
     <link rel="stylesheet" href="../CSS/fontawesome.min.css">
     <script type="module" src="../JS/bootstrap.bundle.min.js"></script>
-    <title>NPL Quadras</title>
+    <script type="module" src="../JS/PopUpBuscar.js"></script>
+    <script type="module" src="../JS/PopUpCadastro.js"></script>
+    <script type="module" src="../JS/PopUpEditar.js"></script>
+    <script type="module" src="../JS/PopUpExcluir.js"></script>
+    <script type="module" src="../JS/PopUpInfo.js"></script>
 
+    <title>Quadras</title>
 </head>
 <body>
-    <?php  
+    <?php
         require '../components/sidebar.php';
-        require '../components/header.php';
+        require '../components/header.php' ;
     ?>
 
     <!-- start main -->
+    <!-- PopUps -->
+            <!-- cadastrar cli/modalClienteente -->
+            <?php include_once "./modalQuadras/cadastroQuadra.php"; ?>
+            <!-- buscar cliente -->
+        <?php include_once "./modalQuadras/buscarQuadra.php"; ?>
+            <!-- editar cliente -->
+        <?php include_once "./modalQuadras/editarQuadra.php"; ?>
+            <!-- excluir cliente -->
+        <?php include_once "./modalQuadras/excluirQuadra.php"; ?>
+            <!-- iformação cliente -->
+        <?php include_once "./modalQuadras/infoQuadra.php"; ?>
+    <!-- buscar cli -->
+                <!-- PopUps -->
+
+
     <div id="main-content">
-        <div class="container">
-            <main>
+        <main>
+            <div class="container">
                 <div class="titulo">
-                    <h1>Cadastro de Quadras</h1>
+                    <h1><strong>Quadras</strong></h1>
                 </div>
-        <!-- start top page -->
-        <div class="top-page">
-            <div class="bts">
-                <button type="submit"href="#">+Nova quadra</button>
-                <input type="text" name="buscar" id="buscar"placeholder="Buscar:"<i class="fa-solid fa-magnifying-glass"></i>>
             </div>
-            
-                 
-            
-            <section class="table">
-                <!-- start tableQuadras  -->
-                    <div class="table-quadra">
+
+                <div class="container-fluids">
+                <section class="top-area">
+                    <div class="adicionar">
+                        <button id='openPopUpCadastro' type="button">+ Novo Cadastro</button>
+                    </div>
+                            
+                    <div class="pesquisar">
+                        <button id='openPopUpBuscar'>Buscar : <i class="fa-solid fa-magnifying-glass"></i></button>
+                    </div>
+                </section>
+                    <!-- start tableCli  -->
+                    <div class="table-clientes">
                         <table class="table table-hover">
                             <thead>
                                 <tr>
                                 <th scope="col">Nome</th>
-                                <th scope="col">Disponibilidade</th>
                                 <th scope="col">Modalidade</th>
-                                <th scope="col">Valor Hora</th>
-                                <th scope="col">Ações</th>
+                                <th scope="col">Disponibilidade</th>
+                                <th scope="col">Valor</th>
                                 </tr>
                             </thead>
                             <tbody>                                    
                             <?php
+                            // foreach ($clientes as $cliente) {
                                 echo"
-                                <td scope ='row'><label for='nomeQuadra'>Quadra Basquete</label></dh>
-                                <td><label for='disponibilidadeQua'>Disponivel</label></td>
-                                <td><label for='modalidadeQua'>Basquete</label></td>
-                                <td><label for='valorHora'>140R$</label></td>
+                                <td scope ='row'><label for='nomeQuadra'>Quadra 1 </label></dh>
+                                <td><label for='modalidadeQuadra'>Society</label></td>
+                                <td><label for='disponibilidadeQuadra'>Disponível</label></td>
+                                <td><label for='valoragendQuadra'>R$ 160,00</label></td>
                                 <td class='icons-item'>
                                     <a id='openPopUpEditar' href='#'><i  class='fa-solid fa-pen-to-square first'></i></a>
                                     <a id='openPopUpExcluir'href='#'><i class='fa-solid fa-trash second'></i></a>
                                     <a id='openPopUpInfo'href='#'><i class='fa-solid fa-circle-info third'></i></a>
                                 </td>";
+                            // }
                             ?>
                             </tbody>
                         </table>
@@ -65,18 +88,22 @@
                             <div class='esquerda'>
                                 <h3>Listando</h3>
                             <?php 
+                            // foreach($pages as $page){
                                 echo"
                                 <div class='labels'><label for='paginaAtual'>1</label> <p>/</p> <label for='totalPaginas'>7</label></div></div>
                                 <div class='direita'><a href='#'><i class='fa-solid fa-arrow-left'></i></a> <label for='paginaAtual'>1</label> <a href='#'><i class='fa-solid fa-arrow-right'></i></a></div>
                                 ";
+                            // }
                             ?>
                         </div>
-            </section>
+                    </div>     
+                </div>
+            </div>
+        </main>
     </div>
-            <!-- end top page -->
+    <footer>
 
-           
-    </main>
-    <!-- end main -->
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
 </html>
--- public/assets/PHP/modalAgendamento/infoAgend.php
+++ public/assets/PHP/modalAgendamento/infoAgend.php
@@ -7,55 +7,55 @@
             <form class="row g-3" action="Clientes.php" method="POST">
                 <div class="col-md-6">
                     <label for="nomeCli" class="form-label">Primeiro Nome</label>
-                    <input type="text" class="form-control" id="nomeCli">
+                    <input type="text" class="form-control" id="nomeCli"disabled>
                 </div>
                 <div class="col-md-6">
                     <label for="sobrenomeCli" class="form-label">Sobrenome</label>
-                    <input type="text" class="form-control" id="sobrenomeCli">
+                    <input type="text" class="form-control" id="sobrenomeCli"disabled>
                 </div>
                 <div class="col-md-4">
                     <label for="dataNascCli" class="form-label">Data de Nascimento</label>
-                    <input type="date" class="form-control" id="dataNascCli">
+                    <input type="date" class="form-control" id="dataNascCli"disabled>
                 </div>
                 <div class="col-md-4">
                     <label for="cpfCli" class="form-label">CPF</label>
-                    <input type="int" class="form-control" id="cpfCli">
+                    <input type="int" class="form-control" id="cpfCli"disabled>
                 </div>
                 <div class="col-md-4">
                     <label for="rgCli" class="form-label">RG (Registro Geral)</label>
-                    <input type="int" class="form-control" id="rgCli">
+                    <input type="int" class="form-control" id="rgCli"disabled>
                 </div>
                 <div class="col-md-6">
                     <label for="cepCli" class="form-label">CEP</label>
-                    <input type="int" class="form-control" id="cepCli">
+                    <input type="int" class="form-control" id="cepCli"disabled>
                 </div>
                 <div class="col-md-6">
                     <label for="cidadeCli" class="form-label">Cidade</label>
-                    <input type="text" class="form-control" id="cidadeCli">
+                    <input type="text" class="form-control" id="cidadeCli"disabled>
                 </div>
                 <div class="col-md-6">
                     <label for="ruaCli" class="form-label">Rua</label>
-                    <input type="text" class="form-control" id="ruaCli">
+                    <input type="text" class="form-control" id="ruaCli"disabled>
                 </div>
                 <div class="col-md-3">
                     <label for="estadoCli" class="form-label">Estado</label>
-                    <input type="text" class="form-control" id="estadoCli">
+                    <input type="text" class="form-control" id="estadoCli"disabled>
                 </div>
                 <div class="col-md-3">
                     <label for="casaCli" class="form-label">Nº Da Residência</label>
-                    <input type="int" class="form-control" id="casaCli">
+                    <input type="int" class="form-control" id="casaCli"disabled>
                 </div>
                 <div class="col-md-6">
                     <label for="contatoCli" class="form-label">Contato</label>
-                    <input type="int" class="form-control" id="contatoCli">
+                    <input type="int" class="form-control" id="contatoCli"disabled>
                 </div>
                 <div class="col-6">
                     <label for="emailCli" class="form-label">Email</label>
-                    <input type="text" value="" class="form-control" id="emailCli">
+                    <input type="text" value="" class="form-control" id="emailCli"disabled>
                 </div>
                 <div class="col-md-12">
                     <label for="descCli" class="form-label">Complementos</label>
-                    <input type="text" class="form-control" id="descCli">
+                    <input type="text" class="form-control" id="descCli"disabled>
                 </div>
                 <div class="col-12">
                     <button type="submit" class="btn btn-primary">Fechar Informações</button>
