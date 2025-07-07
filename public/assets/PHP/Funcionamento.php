bc0d6db46eeb5b95d33903e8c7c1c14fc50ab446
GabrielPedro1408 <gabrielspedro08@gmail.com>
2025-07-07 16:46:58 -0300

    correção de erros dashboard, começando a arrumar a tela de clientes

--- public/assets/PHP/Funcionamento.php
+++ public/assets/PHP/Funcionamento.php
@@ -20,6 +20,9 @@
 
 </head>
 <body>
+<script>
+  localStorage.setItem('activeItem', 'funcionamento');
+</script>
 <div class="full-content">
     <?php require '../components/sidebar.php';?>
     <div id="main-content">
@@ -48,6 +51,7 @@
         </main>
     </div>
 </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+<script src="../components/sidebar.js"></script>    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
 </html>
