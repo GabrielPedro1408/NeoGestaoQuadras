d3916d028ab473a0f8d4849c99875bb76a3e089e
vitorMalevichi07 <vmalevichi@gmail.com>
2025-09-25 23:29:42 -0300

    Commit de Funcionamento
    Nesse commit eu fiz funcionar toda a parte de funcionamento desde o cadastro da empresa.
    Arrumei a query do card de contas a pagar e receber.
    Coloquei o nome da empresa e o do usuario no dashboard e header

--- public/assets/PHP/CRUD-Empresa/createEmpresa.php
+++ public/assets/PHP/CRUD-Empresa/createEmpresa.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include_once __DIR__ . '/../conexao.php';
-if (isset($_POST['Cadastrar'])){
+if (isset($_POST['Cadastrar'])) {
     //post dados empresa
     $razaoSocial = $_POST['razaoSocial'];
     $emailEmpresa = $_POST['emailEmpresa'];
@@ -10,13 +10,13 @@ if (isset($_POST['Cadastrar'])){
     $cepEmpresa = $_POST['cepEmpresa'];
     $numeroEmpresa = $_POST['numeroEmpresa'];
     $cidadeEmpresa = $_POST['cidadeEmpresa'];
-    $ruaEmpresa= $_POST['ruaEmpresa'];
+    $ruaEmpresa = $_POST['ruaEmpresa'];
     $ufEmpresa = $_POST['ufEmpresa'];
     //post dados usuario
     $nomeUsuario = $_POST['nomeUsuario'];
     $senhaUsuario = $_POST['senhaUsuario'];
-    try{
-        echo 'a' ;
+    try {
+        echo 'a';
         //Sql Empresa
         $cadastraEmpresa = $pdo->prepare("INSERT into empresa(razão_social,email,telefone,cnpj,cep,uf,cidade,rua,numero)
         values(:razaoSocial,:emailEmpresa,:telefoneEmpresa,:cnpjEmpresa,:cepEmpresa,:ufEmpresa,:cidadeEmpresa,:ruaEmpresa,:numeroEmpresa)");
@@ -33,8 +33,8 @@ if (isset($_POST['Cadastrar'])){
             ':ruaEmpresa' => $ruaEmpresa,
             ':numeroEmpresa' => $numeroEmpresa
         ));
-        
-        if(!$resultEmpresa){ //Caso o cadastro da empresa falhe
+
+        if (!$resultEmpresa) { //Caso o cadastro da empresa falhe
             $_SESSION['message'] = 'Erro ao cadastrar a empresa!';
             $_SESSION['message_type'] = 'danger'; // Bootstrap: vermelho
             header("Location: ../Cadastro.php");
@@ -42,20 +42,34 @@ if (isset($_POST['Cadastrar'])){
         } else {
             //buscaEmpresaRecemCadastrada
             $buscaEmpresa = $pdo->prepare("SELECT id FROM empresa ORDER BY id DESC LIMIT 1");
-            $buscaEmpresa->execute();  
+            $buscaEmpresa->execute();
             $ultimaEmpresa = $buscaEmpresa->fetchAll(PDO::FETCH_ASSOC);
             $idEmpresa = $ultimaEmpresa[0]['id'];//Guarda o id da empresa recem cadastrada
-            
+
+            /* Inico do cadastro do default Horario */
+            $insertHorario = $pdo->prepare("INSERT INTO horarios
+            (id_empresa, h_abertura, h_fechamento, intervalo_tempo)
+            VALUES
+            (:id_empresa, :hAbertura, :hFechamento, :intervaloTempo)
+            ");
+            $insertHorario->execute(array(
+                ':id_empresa' => $idEmpresa,
+                ':hAbertura' => '10:00:00',
+                ':hFechamento' => '22:00:00',
+                ':intervaloTempo' => 0
+            ));
+            /* Fim do cadastro do default Horario */
+
             //Sql Usuario
             $cadastroUsuario = $pdo->prepare("INSERT INTO usuario (id_empresa, username, senha) VALUES (:id_empresa,:nomeUsuario,:senhaUsuario)");
-            
+
             //Passa os parametros do usuario para inserir no banco de dados e executa
             $resultUsuario = $cadastroUsuario->execute(array(
                 ':id_empresa' => $idEmpresa,
                 ':nomeUsuario' => $nomeUsuario,
-                ':senhaUsuario' => password_hash($senhaUsuario,PASSWORD_BCRYPT)
+                ':senhaUsuario' => password_hash($senhaUsuario, PASSWORD_BCRYPT)
             ));
-            if(!$resultUsuario){
+            if (!$resultUsuario) {
                 $_SESSION['message'] = 'Erro ao cadastrar o usuário!';
                 $_SESSION['message_type'] = 'danger'; // Bootstrap: vermelho
                 header("Location: ../Cadastro.php");
